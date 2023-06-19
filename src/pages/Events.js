4bc45c831d85d57beac8ee79448238f6cf1fd4c1
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-06-19 17:43:15 +0300

    Where Should loader() Code Be Stored?

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -8,3 +8,13 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export async function loader () {
+  const response = await fetch("http://localhost:1237/events");
+
+  if (!response.ok) {
+    //....
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }}
